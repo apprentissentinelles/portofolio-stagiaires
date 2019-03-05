b5f9571cc42d5d4bae67ebdf1eaded09881455ea
Gildas DZINGA <gildasdzinga@hotmail.com>
2019-03-05 10:31:13 +0100

    Premier commit : initialisation projet

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Sentinelles Technologies : accélerateur de succès</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Portofolio Stagiaire</h1>
+				<p>Erwan, Kévin, Killian & Ninon</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- JavaScript Libraries -->
+	<script src="lib/jquery/jquery.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
