7eaa48a3414e863c65038d08b2be86d5830054ea
jacotot killian <jacototk@gmail.com>
2019-03-05 14:38:34 +0100

    14h38

--- pages/killian.php
+++ pages/killian.php
@@ -1,18 +1,12 @@
+	
+
 	<div class="backgroundkillian">
 		<h1 class="bannerkillian">Bienvenue chez Killian | Front-end de père en fils.</h1>
 		<h2 class="bannerkillian">Presentation des projets réalisé au cours de la formation au sein de Groupes Sentinelles.</h2>
 	</div>
 
-	<section class="projetsPendu">
-		<div class ="pendu">
-			<h3>Jeu du pendu</h3>
-			<p>Nous avons conçu un jeu du pendu en Javascript</p>
-		</div>
-	</section>	
-
-
 	<div class="container">
-		<header class="row entete">
+		<header class="row entetependu">
 			<div class="col-lg-12"> 
 				<h1 class="titrepage">
 					Le Pendu - Sentinelles Tech - DWWM
