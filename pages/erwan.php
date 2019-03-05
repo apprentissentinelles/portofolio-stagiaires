2e5c7632d28cb108b6ba1e6c3e9160886c94242b
root <lerwan1@gmail.com>
2019-03-05 15:40:11 +0100

    15h40

--- pages/erwan.php
+++ pages/erwan.php
@@ -1,5 +1,4 @@
-<link href="css/bootstrap.min.css" rel="stylesheet" />
-<link href="../css/style.css" rel="stylesheet">
+
 <!--<body>-->
 <div class="banner backgroundG">
 	<h1 class="banner">Erwan GOASDOUE</h1>
