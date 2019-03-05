af132a825585fe6771e867f84f78bd70c73cc54f
jacotot killian <jacototk@gmail.com>
2019-03-05 11:59:34 +0100

    Modification Killian.php / création dossier images / changement Css

--- pages/killian.php
+++ pages/killian.php
@@ -3,10 +3,21 @@
 <head>
 	<title>Page de Killian</title>
 	<meta charset="utf-8">
+	<link href="../css/style.css" rel="stylesheet">
 </head>
 <body>
-	<div>
-		<h3>Bienvenue chez Killian | Front-end de père en fils.</h3>
+	<div class="banner backgroundkillian">
+		<h1 class="banner">Bienvenue chez Killian | Front-end de père en fils.</h1>
+		<h2>Presentation des projets réalisé au cours de la formation au sein de Groupes Sentinelles.</h2>
 	</div>
+
+	<section class="projets">
+		
+		<div class ="pendu">
+			<h3>Jeu du pendu</h3>
+			<p>Nous avons conçu un jeu du pendu en Javascript</p>
+		</div>
+		<h3>MusikWeb</h3>
+	</section>	
 </body>
 </html>
