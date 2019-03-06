6f2f2d9b431dd58634903961dfbb767e16d8936b
ninon <ninon.touzeau@gmail.com>
2019-03-06 10:03:51 +0100

    ajout photo ninon

--- pages/ninon.php
+++ pages/ninon.php
@@ -1,7 +1,4 @@
 
-<<<<<<< HEAD
-	
-=======
 <!-- 
 	<link href="../css/style.css" rel="stylesheet">
 	<h1 class=grostitreninon><div class="nomninon"> Ninon </div> </br> Cours et Devoirs </h1> -->
@@ -13,10 +10,9 @@
 
 
 <div>
-	<h1 class=grostitreninon><div class="nomninon"> Ninon </div> </br> Cours et Devoirs </h1>
 
-	<h2 class=titresninon> Nos projets </h2>
-		<div class=cours><p> Au sein de cette première partie de formation, nous avons déjà accompli une bonne quantité de travail et acquis de nombreuses connaissances. Nous avons étudié :</p>
+	<h2 class=titresninon> Nos cours </h2>
+		<div class=cours><p> Durant cette première partie de formation, nous avons déjà accompli une bonne quantité de travail et acquis de nombreuses connaissances. Nous avons étudié :</p>
 			<ul>
 				<li>L'algorithmique </li>
 				<li>JavaScript </li>
@@ -31,13 +27,13 @@
 			<ul class=listesninon>
 				<li> I- Présentation du langage HTML </li>
 				<li> II- Les balises 
-					<ul class=listesninon>
+					<ul class="listesninon">
 						<li>a) balises courantes de contenu</li>
 						<li>b) balises courantes de structure</li>
 					</ul>
 				</li>
 				<li> III- La structure de la page 
-					<ul class=listesninon>
+					<ul class="listesninon">
 						<li>a) le doctype</li>
 						<li>b) la balise head</li>
 						<li>c) le body </li>
@@ -46,16 +42,19 @@
 				</li>
 			</ul>
 		<p> Concernant les autres stagiaires, Erwan nous a parlé des fonctions en JavaScript, Kevin des Sélecteurs CSS, et Killian de l'outil Flexbox en CSS. </br>
-		Ces présentations nous ont permis de restituer et synthétiser nos connaissances sur le sujet. </p>
+		Ces présentations nous ont permis de synthétiser et restituer nos connaissances sur le sujet. </p>
 		</div>
 
 
-	<h2 class=titresninon> Qui je suis : <h2>
-		<img class=nuage src="images/nuageninon.png" />
-	<h2 class=titresninon> Ce que m'apporte Sentinelles Technologies <h2>
+	<div class="quijesuis">
+		<h2 class="titresninon">  Qui je suis : </h2>
+		<div>
+			<img class="ninonpres" src="images/ninonpres.jpg"/>
+		</div>
+		<div>
+			<img class="nuage" src="images/nuageninon.png" />
+		</div>
+	</div>
 
-	<h2 class=titresninon> Qui je suis : </h2>
-	<img class=nuage src="images/nuageninon.png" alt="nuage de mots"/>
-	<h2 class=titresninon> Ce que m'apporte Sentinelles Technologies </h2>
 </div>
 
